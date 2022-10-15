a92e3915e323690f6dd4d27822b44876139bbf48
alexraskin <alexraskin@fastmail.fm>
2022-10-15 11:54:54 -0700

    updaate

---
+++ f1_piston/Gear_Shift_Visualization.py
@@ -0,0 +1,92 @@
+import logging
+import os
+
+import fastf1
+import numpy as np
+import streamlit as st
+from matplotlib import cm
+from matplotlib import pyplot as plt
+from matplotlib.collections import LineCollection
+
+logging.basicConfig(level=logging.INFO)
+
+st.set_option("deprecation.showPyplotGlobalUse", False)
+st.set_page_config(
+    page_title="F1 Piston Visualization", page_icon="🏎️", layout="centered"
+)
+st.title("Fastest Lap Gear Shift Visualization 🏎️")
+st.write("All data is from [Fast-F1](https://github.com/theOehrly/Fast-F1)")
+
+st.sidebar.title("Please choose from the following options")
+
+if os.path.exists(path := "../doc_cache"):
+    pass
+else:
+    os.mkdir(path)
+
+fastf1.Cache.enable_cache(path)
+
+
+@st.cache
+def load_session(year: int, grand_prix: str, session: str):
+    session = fastf1.get_session(year, grand_prix, session)
+    session.load()
+    return session
+
+
+session = load_session(2021, "Austrian Grand Prix", "Q")
+
+
+session_type = st.sidebar.selectbox(
+    "Please select the session",
+    (
+        "Practice 1",
+        "Practice 2",
+        "Practice 3",
+        "Sprint Qualifying",
+        "Sprint",
+        "Qualifying",
+        "Race",
+    ),
+)
+
+year = st.sidebar.selectbox("Select Year: ", list(range(2018, 2022)))
+
+
+event = st.sidebar.selectbox(
+    "Please select the Grand Prix:", fastf1.get_event_schedule(year).EventName
+)
+
+
+with st.spinner("Building... 🔨 Please wait..."):
+    try:
+        lap_data = session.laps.pick_fastest()
+        telemetry = lap_data.get_telemetry()
+
+        x = np.array(telemetry["X"].values)
+        y = np.array(telemetry["Y"].values)
+
+        points = np.array([x, y]).T.reshape(-1, 1, 2)
+        segments = np.concatenate([points[:-1], points[1:]], axis=1)
+        gear = telemetry["nGear"].to_numpy().astype(float)
+        cmap = cm.get_cmap("Paired")
+        lc_comp = LineCollection(segments, norm=plt.Normalize(1, cmap.N + 1), cmap=cmap)
+        lc_comp.set_array(gear)
+        lc_comp.set_linewidth(4)
+        plt.gca().add_collection(lc_comp)
+        plt.axis("equal")
+        plt.tick_params(labelleft=False, left=False, labelbottom=False, bottom=False)
+
+        title = st.write(
+            f"Driver: **{lap_data['Driver']}** - EventName: **{session.event['EventName']}** -"
+            + f" EventYear: **{session.event.year}** - LapNumber: **{lap_data['LapNumber']}**"
+        )
+        cbar = plt.colorbar(mappable=lc_comp, label="Gear", boundaries=np.arange(1, 10))
+        cbar.set_ticks(np.arange(1.5, 9.5))
+        cbar.set_ticklabels(np.arange(1, 9))
+
+        st.pyplot(plt.show())
+        st.success("Built! 🏁")
+    except Exception as e:
+        st.error(f"An error occurred, please select another session", icon="⚠️")
+        logging.error(e)
---
+++ f1_piston/pages/Qualifying_Results.py
@@ -0,0 +1,106 @@
+import logging
+import os
+
+import fastf1
+import fastf1.plotting
+import matplotlib.pyplot as plt
+import pandas as pd
+import streamlit as st
+from fastf1.core import Laps
+from timple.timedelta import strftimedelta
+
+logging.basicConfig(level=logging.INFO)
+
+if os.path.exists(path := "../doc_cache"):
+    pass
+else:
+    os.mkdir(path)
+
+fastf1.Cache.enable_cache(path)
+
+
+@st.experimental_memo
+def load_fastf1(year: int, grand_prix: str, session: str):
+    session = fastf1.get_session(year, grand_prix, session)
+    session.load()
+    return session
+
+
+st.set_option("deprecation.showPyplotGlobalUse", False)
+st.set_page_config(
+    page_title="F1 Piston Visualization", page_icon="🏎️", layout="centered"
+)
+st.title("Qualifying results Visualization 🏎️")
+st.write("All data is from [Fast-F1](https://github.com/theOehrly/Fast-F1)")
+
+session_type = st.sidebar.selectbox(
+    "Please select the session",
+    (
+        "Practice 1",
+        "Practice 2",
+        "Practice 3",
+        "Sprint Qualifying",
+        "Sprint",
+        "Qualifying",
+        "Race",
+    ),
+)
+
+year = st.sidebar.selectbox("Select Year: ", list(range(2018, 2022)))
+
+event = st.sidebar.selectbox(
+    "Please select the Grand Prix:", fastf1.get_event_schedule(year).EventName
+)
+
+with st.spinner("Building... 🔨 Please wait..."):
+    try:
+        fastf1.plotting.setup_mpl(
+            mpl_timedelta_support=True, color_scheme=None, misc_mpl_mods=False
+        )
+
+        session = load_fastf1(year, event, session_type)
+        drivers = pd.unique(session.laps["Driver"])
+
+        list_fastest_laps = list()
+        for drv in drivers:
+            drvs_fastest_lap = session.laps.pick_driver(drv).pick_fastest()
+            list_fastest_laps.append(drvs_fastest_lap)
+        fastest_laps = (
+            Laps(list_fastest_laps).sort_values(by="LapTime").reset_index(drop=True)
+        )
+
+        pole_lap = fastest_laps.pick_fastest()
+        fastest_laps["LapTimeDelta"] = fastest_laps["LapTime"] - pole_lap["LapTime"]
+
+        team_colors = list()
+        for index, lap in fastest_laps.iterlaps():
+            color = fastf1.plotting.team_color(lap["Team"])
+            team_colors.append(color)
+
+        fig, ax = plt.subplots()
+        ax.barh(
+            fastest_laps.index,
+            fastest_laps["LapTimeDelta"],
+            color=team_colors,
+            edgecolor="grey",
+        )
+        ax.set_yticks(fastest_laps.index)
+        ax.set_yticklabels(fastest_laps["Driver"])
+
+        ax.invert_yaxis()
+
+        ax.set_axisbelow(True)
+        ax.xaxis.grid(True, which="major", linestyle="--", color="black", zorder=-1000)
+
+        lap_time_string = strftimedelta(pole_lap["LapTime"], "%m:%s.%ms")
+
+        plt.suptitle(
+            f"{session.event['EventName']} {session.event.year} Qualifying\n"
+            f"Fastest Lap: {lap_time_string} ({pole_lap['Driver']})"
+        )
+
+        st.pyplot(plt.show())
+        st.success("Built! 🏁")
+    except Exception as e:
+        st.error(f"An error occurred, please select another session", icon="⚠️")
+        logging.error(e)
